4a4df63db9725759abc5339d899d3f203568e439
rmit-s3427467-Donghai-Liu <s3427467@student.rmit.edu.au>
2017-10-09 06:05:04 +1100

    ssh key management

--- a2.pp
+++ a2.pp
@@ -48,4 +48,13 @@ user { 'wilma':
         groups => ['trucks','cars','ambulances']
 
 }
+
+ssh_authorized_key { 'wilma@sshkey.com':
+  user => 'wilma',
+  type => 'ssh-rsa',
+  key  => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAy5mtOAMHwA2ZAIfW6Ap70r+I4EclYHEec5xIN59ROUjss23Skb1OtjzYpVPaPH8mSdSmsN0JHaBLiRcu7stl4O8D8zA4mz/vw32yyQ/Kqaxw8l0K76k6t2hKOGqLTY4aFbFISV6GDh7MYLn8KU7cGp96J+caO5R5TqtsStytsUhSyqH+iIDh4e4+BrwTc6V4Y0hgFxaZV5d18mLA4EPYKeG5+zyBCVu+jueYwFqM55E0tHbfiaIN9IzdLV+7NEEfdLkp6w2baLKPqWUBmuvPF1Mn3FwaFLjVsMT3GQeMue6b3FtUdTDeyAYoTxrsRo/WnDkS6Pa3YhrFwjtUqXfdaQ==',
+}
+
+
+
 }
