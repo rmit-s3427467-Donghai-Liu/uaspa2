a613b243c9747c9dd3a303282d3bd160c6c4ab70
rmit-s3427467-Donghai-Liu <s3427467@student.rmit.edu.au>
2017-10-12 06:46:43 +1100

    Forgot something

--- path.pp
+++ path.pp
@@ -1,3 +1,4 @@
+#add path to global.
 class path{
 exec { 'export PATH=$PATH:/usr/local/bin' :
 path => ['/usr/bin','/usr/sbin','/usr/local/bin'],
--- schedule.pp
+++ schedule.pp
@@ -7,12 +7,3 @@ changes => [ 'set runiterval = 900', ]
 }
 }
 
-
-schedule { 'hourly' :
-period => hourly,
-repeat => 3,
-}
-exec { 'sudo /usr/local/bin/puppet agent -t' :
-		schedule => 'hourly',
-		path => ['/usr/bin','/usr/sbin','/usr/local/bin'],
-	}
